22395040ffdf6db6849f02eccb2c1c6e28b73402
karthikeyan-1234 <mail2karthikkn@gmail.com>
2023-11-06 15:07:12 +0530

    Modified Navbar to remove extras

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -28,17 +28,6 @@
               </v-btn>
         </v-app-bar>
        <v-navigation-drawer  v-model="drawer" dark app class="red darken-4">
-           <v-layout column align-center>
-                <v-flex class="mt-5"> 
-                     <v-avatar size="100">
-                             <img src="/img1.png" alt="">
-                     </v-avatar>
-                     <p class="white--text subheading mt-1 text-center">Username</p>
-                </v-flex>
-                <v-flex class="mt-4 mb-4">
-                 <Popup />
-                </v-flex>
-           </v-layout>
            <v-list flat>
                <v-list-item v-for="link in links"  :key="link.text" router :to="link.route" active-class="border">
                    <v-list-item-action>
